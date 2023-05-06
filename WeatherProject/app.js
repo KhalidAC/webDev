3ea0e85cdd547a966adce27e8d701269f8f2f76d
Khalid A <beardokya@gmail.com>
2023-05-05 23:38:47 -0400

    fixing minor bugs, adding city search

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,15 +1,39 @@
 const express= require ("express");
 const https = require("https");
+const bodyParser = require("body-parser");
+
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", function(req, res){
-    const url = "https://api.openweathermap.org/data/3.0/weather?q=Ottawa&units=metric&appid=9f01ac8b9dd864c81bf8df4a6b0d9453";
-    https.get(url,function(response){
-        console.log(response.statusCode);
-    });
-    res.send("Server is running...");
+    res.sendFile(__dirname +"/index.html");
 });
+
+app.post("/", function(req, res){
+    const queryCity = req.body.cityName;
+    const apiKey = "";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + queryCity +"&units=metric&appid="+apiKey;
+
+    https.get(url,function(response){
+
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon=weatherData.weather[0].icon;
+            const imageURL= "http://openweathermap.org/img/wn/" + icon +"@2x.png";
+
+            console.log(icon);
+
+            res.write("<h1>The current Temperature in "+ queryCity +" is: " + temp + " degrees Celsius</h1>");
+            res.write("<p>Current Weather: "+ weatherDescription+ "</p>");
+            res.write("<img src=" +imageURL +">");
+            res.send();
+        });
+    });
+
+})
 
 
 app.listen(3000, function(){
